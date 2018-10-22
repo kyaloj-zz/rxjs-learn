15fcc7a03105014581d74785d991dfe61c73efb0
Justus <mj.kyalo@gmail.com>
2018-10-22 14:41:16 +0300

    subjects

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
+// Observable
 let observable = Observable.create((observer:any) => {
   observer.next("Hey ya'll")
   observer.next("How you doing?")
@@ -13,6 +14,31 @@ let observer = observable.subscribe(
 );
 
 setTimeout(() => observer.unsubscribe(), 6001);
+
+
+// Subject
+
+let subject = new Subject();
+
+
+subject.subscribe(
+  (data: any) => addItem('Observer 1 ' + data),
+  (error: any) => addItem(error),
+  () => addItem('Observer1 Done')
+);
+
+subject.next('The first thing has been sent')
+
+var observer2 = subject.subscribe(
+    data => addItem('Observer 2: '+ data)
+)
+
+subject.next('The second thing has been sent')
+subject.next('A third thing has been sent')
+
+observer2.unsubscribe();
+
+subject.next('A final thing has been sent')
 
 function addItem(val:any) {
     let node = document.createElement("li");
